3bf72a917b1729de65f0234c352d2e27f4b78453
Styles <fs596@student.aru.ac.uk>
2025-11-03 11:53:05 +0000

    Fixed camera broken on higher res monitors.

--- Src/SCR_CameraController.gd
+++ Src/SCR_CameraController.gd
@@ -7,7 +7,7 @@ var LerpSpeed:float = 12.0
 var TargetPosition:Vector3 = Vector3.ZERO
 var TargetRotation:Vector3 = Vector3(0,-45,0)
 
-var viewport_size = Vector2(1280,720)
+var viewport_size = Vector2(3838,2158)
 var viewport:Viewport
 
 var CurrentScroll:float = 0.0
@@ -17,13 +17,18 @@ var RotateTween:Tween
 @export var CameraHolder:Node3D
 @export var Camera:Node3D
 
+var window_size = DisplayServer.window_get_size()
+
 var delta:float
 
+@export var MousePositionText:RichTextLabel
+@export var ShouldMoveText:RichTextLabel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 	viewport = get_viewport()
-	viewport_size = get_viewport().size
+	viewport_size = viewport.get_visible_rect().size
+	print(viewport_size)
 	pass # Replace with function body.
 
 
@@ -33,25 +38,31 @@ func _process(delta: float) -> void:
 	CameraHolder.position = lerp(CameraHolder.position,Vector3(0,-CurrentScroll*1.5,-CurrentScroll* 0.5),LerpSpeed*delta)
 	
 	CameraHolder.rotation_degrees.x = lerp(CameraHolder.rotation_degrees.x,-CurrentScroll * 8, LerpSpeed * delta)
-	
 	var local_mouse_pos = viewport.get_mouse_position()
+	MousePositionText.text = str(local_mouse_pos)
+	print(viewport_size.x)
 	if local_mouse_pos.x < threshold:
 		#TargetPosition.z -= step
 		#TargetPosition.x -= step
 		TargetPosition -= Camera.global_basis.x * step
+		ShouldMoveText.text = "TRUE L"
 	elif local_mouse_pos.x > viewport_size.x - threshold:
 		#TargetPosition.z += step
 		#TargetPosition.x += step
 		TargetPosition += Camera.global_basis.x * step
+		ShouldMoveText.text = "TRUE R"
+		
 	if local_mouse_pos.y < threshold:
 		#TargetPosition.x += step
 		#TargetPosition.z -= step
 		TargetPosition -= global_basis.z * step
-	elif local_mouse_pos.y > viewport_size.y - threshold:
+		ShouldMoveText.text = "TRUE U"
+	if local_mouse_pos.y > viewport_size.y - threshold:
 		#TargetPosition.x -= step
 		#TargetPosition.z += step
 		TargetPosition += global_basis.z * step
-		
+		ShouldMoveText.text = "TRUE D"
+	
 	global_position = lerp(global_position, TargetPosition, LerpSpeed * delta)
 	
 	rotation.y = lerp_angle(rotation.y,deg_to_rad(TargetRotation.y),LerpSpeed* delta)
