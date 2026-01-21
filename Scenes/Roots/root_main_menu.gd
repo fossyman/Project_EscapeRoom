f4d471c90529c43665f6c9a0cdc485228269e05e
Harwood <kh951@student.aru.ac.uk>
2026-01-21 15:50:52 +0000

    UI started

---
+++ Scenes/Roots/root_main_menu.gd
@@ -0,0 +1,19 @@
+class_name RT_MainMenu extends Node
+
+
+
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
