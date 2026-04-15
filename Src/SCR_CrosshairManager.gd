23ba00a0423a0f73afd88683a63007d231a0d208
Styles <fs596@student.aru.ac.uk>
2026-04-15 17:22:42 +0100

    Safety Push

---
+++ Src/SCR_CrosshairManager.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+@export var CrosshairNorthPin:Node3D
+@export var CrosshairSouthPin:Node3D
+@export var CrosshairEastPin:Node3D
+@export var CrosshairWestPin:Node3D
+
+@export var CrosshairSpacing:float
+
+@export var Frequency:float = 10
+@export var Amplification:float = 0.1
+
+var SineTime:float = 0.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	SineTime += delta
+	var sine = sin(SineTime * Frequency)* Amplification
+	CrosshairNorthPin.position.z = CrosshairSpacing + sine
+	CrosshairSouthPin.position.z = -CrosshairSpacing + -sine
+
+	CrosshairEastPin.position.x = CrosshairSpacing + sine
+	CrosshairWestPin.position.x = -CrosshairSpacing + -sine
+	pass
